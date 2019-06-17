31adebfe5dacfc772207c26ed82e04cbf527d815
Andi <familie.mayer@aon.at>
2019-06-17 19:54:09 +0200

    initial commit

---
+++ src/main/java/com/example/andi/webservicestestingpoc/WebservicesTestingPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.andi.webservicestestingpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesTestingPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebservicesTestingPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/andi/webservicestestingpoc/WebservicesTestingPocApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.andi.webservicestestingpoc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebservicesTestingPocApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
